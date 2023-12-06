fd1671c3d037f3dbfb165efdc251e4a3d8dda2c0
elicharlese <elicharles.e@gmail.com>
2023-12-05 22:34:51 -0800

    Cleanup

---
+++ app/contract/target/debug/deps/hello_near-4375c8115c57919a.d
@@ -0,0 +1,5 @@
+/Users/elicharlese/Team CEC Dropbox/Elias Estrada/ECE/Professional/Trader/STOKE/STOKE/app/contract/target/debug/deps/hello_near-4375c8115c57919a.rmeta: src/main.rs
+
+/Users/elicharlese/Team CEC Dropbox/Elias Estrada/ECE/Professional/Trader/STOKE/STOKE/app/contract/target/debug/deps/hello_near-4375c8115c57919a.d: src/main.rs
+
+src/main.rs:
---
+++ app/contract/target/debug/deps/hello_near-5454601c0a56cf46.d
@@ -0,0 +1,5 @@
+/Users/elicharlese/Team CEC Dropbox/Elias Estrada/ECE/Professional/Trader/STOKE/STOKE/app/contract/target/debug/deps/hello_near-5454601c0a56cf46.rmeta: src/main.rs
+
+/Users/elicharlese/Team CEC Dropbox/Elias Estrada/ECE/Professional/Trader/STOKE/STOKE/app/contract/target/debug/deps/hello_near-5454601c0a56cf46.d: src/main.rs
+
+src/main.rs:
